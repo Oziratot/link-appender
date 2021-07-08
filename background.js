fafd4227bd82e27429d4dfe8130e096c1338ff41
Roman Zavaliy <romasty_92@mail.ru>
2021-07-09 00:07:11 +0300

    to do: background.js

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 chrome.commands.onCommand.addListener(() => {
-    chrome.tabs.create({ url: "https://www.google.com/" });
+  chrome.scripting.executeScript({
+    code: "window.getSelection().toString();"
+  }, function(selection) {
+    chrome.tabs.create({
+      url: "https://www.google.com/search?q=" + selection[0]
+    });
   });
+});
