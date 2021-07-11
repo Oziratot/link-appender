9fe984f52564822165090f8c764e02c30b9d0f4b
Roman Zavaliy <romasty_92@mail.ru>
2021-07-11 16:48:01 +0300

    switched to manifest v2

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
-chrome.commands.onCommand.addListener(() => {
-  chrome.scripting.executeScript({
-    code: "window.getSelection().toString();"
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.executeScript({
+      code: "window.getSelection().toString(); "
   }, function(selection) {
-    chrome.tabs.create({
-      url: "https://www.google.com/search?q=" + selection[0]
-    });
+      chrome.tabs.create({
+          url: "http://music.yandex.ru/search?text=" + selection[0]
+      });
   });
 });
